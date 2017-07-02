3b74d6ba7f455f727ad9e15213d6805e5363f416
tldrd0117 <tldrd0117@gmail.com>
2017-07-02 21:56:13 +0900

    observable

--- app/src/main/java/com/example/iseongjae/MainActivity.java
+++ app/src/main/java/com/example/iseongjae/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.iseongjae.rxjavatest.R;
+
 /**
  * Created by iseongjae on 2017. 6. 28..
  */
@@ -12,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
